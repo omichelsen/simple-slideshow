da193d41fe6caad3e28288c895cfd80b8cf668e9
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-03-19 22:38:18 +0100

    Added tests for full coverage.

--- test/slideshow.js
+++ test/slideshow.js
@@ -1,27 +1,49 @@
 describe('simple-slideshow', function () {
-    var elm;
+    var elm,
+        options = {timeout: 10};
 
     function createElement(tag, props) {
         var elm = document.createElement(tag);
         for (var key in props) {
-        	elm.setAttribute(key, props[key]);
+            elm.setAttribute(key, props[key]);
         }
         return elm;
     }
 
     beforeEach(function () {
-    	var ul = createElement('ul', {'class': 'testshow'}),
-    		li = createElement('li'),
-    		img = createElement('img');
-    	li.appendChild(img);
-    	ul.appendChild(li);
+        var ul = createElement('ul', {'class': 'testshow'}),
+            li0 = createElement('li'),
+            li1 = createElement('li');
+        li0.appendChild(createElement('img', {title: 'Title1', alt: 'Alt1'}));
+        li1.appendChild(createElement('img', {title: 'Title1', alt: 'Alt1'}));
+        ul.appendChild(li0);
+        ul.appendChild(li1);
         document.body.appendChild(ul);
-        elm = new SlideShow('.testshow');
-        console.log(elm);
+        elm = new SlideShow('.testshow', options);
     });
 
     it('should be classy', function () {
         expect(elm.classList).toContain('slideshow');
+        expect(elm.classList).toContain('preload');
+        expect(elm.querySelector('li:first-child').classList).toContain('show-animation');
     });
 
+    it('should add caption', function () {
+        expect(elm.querySelector('.caption')).not.toBeNull();
+    });
+
+    it('should remove preload', function (done) {
+        setTimeout(function () {
+            expect(elm.classList).not.toContain('preload');
+            done();
+        });
+    });
+
+    it('should change to next slide', function (done) {
+        setTimeout(function () {
+            expect(elm.querySelector('li:first-child').classList).not.toContain('show-animation');
+            expect(elm.querySelector('li:last-child').classList).toContain('show-animation');
+            done();
+        }, options.timeout);
+    });
 });
